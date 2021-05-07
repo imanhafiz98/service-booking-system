73561a0b43f77b0928bdf284904f82c120dbb8d8
imanhafiz98 <imanhafiz333@gmail.com>
2021-05-07 14:53:40 +0800

    Create InvoiceController folder under client & Add function index

---
+++ app/Http/Controllers/client/InvoiceController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\client;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Service;
+use App\Models\Location;
+use App\Models\Category;
+use App\Models\Req;
+
+class InvoiceController extends Controller
+{
+    public function index()
+    {
+        return view('client.invoices.index');
+    }
+}
